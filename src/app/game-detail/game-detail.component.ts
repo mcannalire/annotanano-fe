6545119c42b3b901048c3e997bc254f09d064923
mcannalire <lokad90@gmail.com>
2021-01-10 19:05:55 +0100

    click on suggested

--- src/app/game-detail/game-detail.component.ts
+++ src/app/game-detail/game-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { of } from 'rxjs';
 import { flatMap } from 'rxjs/operators';
 import { GamesService } from '../services/games.service';
 
@@ -19,7 +20,7 @@ export class GameDetailComponent {
   totalHours;
   gameName;
   similarGames;
-  constructor(private gamesService: GamesService, private route: ActivatedRoute, private sanitizer: DomSanitizer){
+  constructor(private gamesService: GamesService, private route: ActivatedRoute, private sanitizer: DomSanitizer, private router: Router){
     this.mediaAnnotanano = 0;
     this.totalHours = 0;
     this.route.queryParams.pipe(
@@ -74,5 +75,60 @@ export class GameDetailComponent {
     this.trustedUrl = this.sanitizer.bypassSecurityTrustUrl(this.gameDetail.clip.clip);
   }
 
+  toGameDetail(game){
+    //this.router.navigate(['/game-detail'], {queryParams: {idGame: game.id, name: game.name}});
+    this.listPerson = [];
+    this.mediaAnnotanano = 0;
+    this.totalHours = 0;
+    this.isClip = null;
+    this.refresh(game.id, game.name);
+  }
+
+  refresh(id, name){
+    of({}).pipe(
+      flatMap((params) =>{
+        return this.gamesService.getSimilarResult(id);
+      }),
+      flatMap((data) => {
+        if(data && data['results'] && data['results'].length > 0){
+          this.similarGames = data['results'];
+        }
+        return this.gamesService.getResults(name);
+      }),
+      flatMap((data) => {
+        if(data && data['results'] && data['results'].length > 0){
+          const results = data['results'];
+          results.forEach((game) => {
+            if(game.id == id){
+              this.gameDetail = game;
+            }
+          })
+        }
+        return this.gamesService.getAll();
+      })
+    ).subscribe((p:any) => {
+      if(p){
+        p.forEach(person => {
+          if(person.gamesThisYear){
+            person.gamesThisYear.forEach(element => {
+              if(element.idGame == id){
+                this.listPerson.push(person);
+                this.mediaAnnotanano += element.rating;
+                this.totalHours += element.hours;
+              }
+            });
+          }
+        });
+
+        if(this.listPerson && this.listPerson.length > 0){
+          this.mediaAnnotanano = this.mediaAnnotanano / this.listPerson.length;
+          this.mediaAnnotanano = Math.round(this.mediaAnnotanano * 10) / 10
+        }
+       
+      }
+      this.isClip = this.gameDetail.clip ? this.gameDetail.clip.clip : null;
+      
+    })
+  }
   
 }
